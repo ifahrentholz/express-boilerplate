6db0cd073dfb87a7580974a6d52613a665f9c22e
ifahrentholz <ingowork@gmail.com>
2015-06-27 20:18:20 +0200

    refactor file/folder structure oriented on RoR

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
   app.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('home/index', { title: 'Express' });
   });
 };
